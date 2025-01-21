d46cd74a3062cab0e6f86ee027d125400fef8a58
Danny Nguyen <nguyenduongpa@gmail.com>
2025-01-21 08:10:23 -0800

    d

--- lyft_interview.js
+++ lyft_interview.js
@@ -2,17 +2,22 @@
 
 /* Return true if driver has driven less than 12 hours since their last 8 hour break */
 function canDrive(history, currentTime) {
+    if (!history || history.length === 0 || currentTime == null) return false;
+
     let length = history.length;
-    let last = 0; current_duration = 0; break_total = 0;
+    let last = 0; hours_worked = 0; last_break = 0;
+
+    if (currentTime - history[length - 1][1] >= 8) return true;
 
     for (let i = 0; i < length; i++) {
         if (last > 0) {
-            break_total = history[i][0] - last;
+            last_break = history[i][0] - last;
         }
-        current_duration = history[i][1] - history[i][0];
-        console.log("[" + "time: " + current_duration + " hours, last break: " + break_total + " hours]");
-        if (break_total < 8 && current_duration >= 12) {
+        hours_worked += history[i][1] - history[i][0];
+        if (hours_worked >= 12 && last_break < 8) {
             return false;
+        } else if (last_break >= 8) {
+            hours_worked = 0;
         }
         last = history[i][1];
     }
@@ -20,13 +25,11 @@ function canDrive(history, currentTime) {
 }
 
 var history1 = [[2, 6], [14, 24]]; // true
-var history2 = [[2, 8], [12, 16], [20, 22]]; // true
+var history2 = [[2, 8], [12, 16], [20, 22]]; // false
 var history3 = [[2, 8], [10, 22]]; // false
 var history4 = [[0, 4], [6, 10], [12, 16]]; // true
-var history5 = [[0, 13]]; // false
 
 console.log(canDrive(history1, 24));
 console.log(canDrive(history2, 24));
 console.log(canDrive(history3, 24));
 console.log(canDrive(history4, 24));
-console.log(canDrive(history5, 24));
